1ac100c9583b3d3306639d70f6fa05612863b27e
charlotte-ngs <peter.vonrohr@icloud.com>
2015-10-14 11:00:47 +0200

    First final version of u3

--- inst/extdata/w5/generateOriginalGenotypeData.R
+++ inst/extdata/w5/generateOriginalGenotypeData.R
@@ -9,11 +9,13 @@
 
 ### # define a few constants
 ### # directory where the original data can be found
-sDatDir <- "~/Google Drive/eBooks/GWH2013_Genome-Wide_Association_Studies_and_Genomic_Prediction/978-1-62703-446-3/Chapter10"
+sDatDir <- ifelse(.Platform$OS.type == "windows",
+                  "C:/Daten/pvr/Google Drive/eBooks/GWH2013_Genome-Wide_Association_Studies_and_Genomic_Prediction/978-1-62703-446-3/Chapter10",
+                  "~/Google Drive/eBooks/GWH2013_Genome-Wide_Association_Studies_and_Genomic_Prediction/978-1-62703-446-3/Chapter10")
 ### # name of the file with original data
 sGenDatFn <- "Genotypes.dat"
 ### # number of lines to be read from orignial data file
-nDataLine <- 21
+nDataLine <- 31
 ### # column indices to extract
 nStartCol <- 2
 nEndCol <- 5
@@ -39,7 +41,10 @@ sGetHeaderLine <- function(psGenDataLine, pnHeaderLineIdx, psColSplit, pnStartCo
 ##############################
 ### # code starts here ....
 ### # save the current working directory
-zlwd <- getwd()
+#zlwd <- getwd()
+zlwd <- ifelse(.Platform$OS.type == "windows",
+               "C:/Daten/GitHub/Zuechtungslehre",
+               "~/Data/Projects/GitHub/Zuechtungslehre")
 
 ### # change wd to where genotype.dat is located
 setwd(sDatDir)
@@ -81,13 +86,24 @@ vecYTrue <- matDesignX %*% vecBetaTrue + rnorm(n = nrow(matDesignX), mean = nMea
 
 ### # conversion to a dataframe
 id <- matGenoDat[,1]
-locus1 <- matGenoDat[,2]
-locus2 <- matGenoDat[,3]
-locus3 <- matGenoDat[,4]
+locus1 <- matDesignX[,2]/10
+locus2 <- matDesignX[,3]/10
+locus3 <- matDesignX[,4]/10
 y <- vecYTrue
 dfSimGenPhenDat <- data.frame(id,locus1, locus2, locus3, y, stringsAsFactors = FALSE)
 
+### # fit lm
+fitMultReg <- lm( y ~ locus1 + locus2 + locus3, data = dfSimGenPhenDat)
+summary(fitMultReg)
+
+### # single regressions
+summary(lm(y ~ locus1, data = dfSimGenPhenDat))
+summary(lm(y ~ locus2, data = dfSimGenPhenDat))
+summary(lm(y ~ locus3, data = dfSimGenPhenDat))
 
 
-
+### # re-set working directory
 setwd(zlwd)
+
+### # write df to csv file
+write.csv2(dfSimGenPhenDat, file = "inst/extdata/w5/simgenphen.csv", quote = FALSE, row.names = FALSE)
